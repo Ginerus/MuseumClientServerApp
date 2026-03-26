44c3b10c36b966ac2d4010d1fa2e8cefe39750ce
Герман Исаев <germanisaev422@gmail.com>
2026-03-26 23:06:21 +0300

    Добавлен DbContext

---
+++ MuseumServer/Data/MuseumContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MuseumServerApi.Models;
+
+namespace MuseumServer.Data
+{
+    public class MuseumContext : DbContext
+    {
+        public MuseumContext(DbContextOptions<MuseumContext> options) : base(options) { }
+
+        public DbSet<Session> Sessions { get; set; } = null!;
+
+        // Для будущих расширений:
+        // public DbSet<Department> Departments { get; set; } 
+        // public DbSet<Exhibit> Exhibits { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Конфигурация Session
+            modelBuilder.Entity<Session>(entity =>
+            {
+                entity.HasKey(e => e.SessionId);
+                entity.Property(e => e.Token).IsRequired();
+                entity.Property(e => e.UserType).IsRequired();
+                entity.Property(e => e.CreatedAt).IsRequired();
+                entity.Property(e => e.LastAccess).IsRequired();
+            });
+        }
+    }
+}
