f2b90afb589423976a2258aa85ddc0d745393f1a
Герман Исаев <germanisaev422@gmail.com>
2026-03-26 22:56:05 +0300

    ДОбавлен файл с кодом для очистки сесий из бд

---
+++ MuseumServer/Services/SessionCleanupService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace MuseumServer.Services
+{
+    public class SessionCleanupService : BackgroundService
+    {
+        private readonly SessionService _sessionService;
+
+        public SessionCleanupService(SessionService sessionService)
+        {
+            _sessionService = sessionService;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _sessionService.CleanupOldSessions();
+                await Task.Delay(TimeSpan.FromMinutes(10), stoppingToken);
+            }
+        }
+    }
+}
