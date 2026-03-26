f3cc998de05a76df29ddb834ca8112c9ad2b8ae7
Герман Исаев <germanisaev422@gmail.com>
2026-03-26 22:56:59 +0300

    Изменён файл точки входа

--- MuseumServer/Program.cs
+++ MuseumServer/Program.cs
@@ -1,4 +1,6 @@
 
+using MuseumServer.Services;
+
 namespace MuseumServer
 {
     public class Program
@@ -7,26 +9,18 @@ namespace MuseumServer
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Add services
+            builder.Services.AddSingleton<SessionService>();
+            builder.Services.AddHostedService<SessionCleanupService>();
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.MapControllers();
 
