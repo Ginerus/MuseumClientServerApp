34633484ec532591b7e60fcdc3cd971386df173d
Герман Исаев <germanisaev422@gmail.com>
2026-03-26 23:05:20 +0300

    Добавлен файл сесии для бд

---
+++ MuseumServer/Models/Session.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MuseumServer.Models
+{
+    public class Session
+    {
+        public int SessionId { get; set; } // PK
+        public string Token { get; set; } = null!;
+        public string UserType { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastAccess { get; set; }
+    }
+}
