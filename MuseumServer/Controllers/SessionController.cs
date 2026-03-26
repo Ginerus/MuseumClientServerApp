2ff53a7bef532460e6e83839ae2514cee234a425
Герман Исаев <germanisaev422@gmail.com>
2026-03-26 22:57:54 +0300

    Добавлен контролер регистрации сессий

---
+++ MuseumServer/Controllers/SessionController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using MuseumServer.Models;
+using MuseumServer.Services;
+
+namespace MuseumServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SessionController : ControllerBase
+    {
+        private readonly SessionService _sessionService;
+
+        public SessionController(SessionService sessionService)
+        {
+            _sessionService = sessionService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterRequest req)
+        {
+            if (req.UserType == "admin" && !_sessionService.ValidateAdminPassword(req.Password))
+                return BadRequest(new Response<object>("error", null, "ADMIN_AUTH_FAIL"));
+
+            var token = _sessionService.CreateSession(req.UserType);
+            return Ok(new Response<object>("ok", new { token }));
+        }
+    }
+}
