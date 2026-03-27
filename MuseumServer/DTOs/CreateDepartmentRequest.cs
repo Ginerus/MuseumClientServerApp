60fb9cadd49904daf032b1e8bcbb6a8e760f49c9
Герман Исаев <germanisaev422@gmail.com>
2026-03-27 09:53:46 +0300

    DTO оздания нового отдела

---
+++ MuseumServer/DTOs/CreateDepartmentRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace MuseumServer.DTOs
+{
+    // Для создания нового отдела
+    public class CreateDepartmentRequest
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
