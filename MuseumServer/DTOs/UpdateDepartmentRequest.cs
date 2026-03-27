dc2ed9eae9cc01e6bb05ed42b5370578b28300b6
Герман Исаев <germanisaev422@gmail.com>
2026-03-27 09:55:13 +0300

    UpdateDepartmentRequest для изменения существующего отдела

---
+++ MuseumServer/DTOs/UpdateDepartmentRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace MuseumServer.DTOs
+{
+    // Для изменения существующего отдела
+    public class UpdateDepartmentRequest
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
