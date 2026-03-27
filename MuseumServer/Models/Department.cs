39e2d69a95756562b9de8ff8d84263ac0b9752be
Герман Исаев <germanisaev422@gmail.com>
2026-03-27 10:33:03 +0300

    удалён  List<Exhibit>? Exhibits, лпанировался ранее, но аказался бесполезен и удалён как мусор

--- MuseumServer/Models/Department.cs
+++ MuseumServer/Models/Department.cs
@@ -6,8 +6,5 @@
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public string? ImagePath { get; set; }
-
-        // Навигационное свойство — список экспонатов в этом отделе
-        public List<Exhibit>? Exhibits { get; set; }
     }
 }
