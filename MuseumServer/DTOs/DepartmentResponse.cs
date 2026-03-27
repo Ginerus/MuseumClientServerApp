edcae030e522d246c00a3af22e48fffdd02cadee
Герман Исаев <germanisaev422@gmail.com>
2026-03-27 09:56:02 +0300

    DepartmentResponse - для  вывода отдела

---
+++ MuseumServer/DTOs/DepartmentResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace MuseumServer.DTOs
+{
+    // Ответ при выводе отдела (id)
+    public class DepartmentResponse
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
