fd2209e81540a6884378bc5599afb76316a17db0
Герман Исаев <germanisaev422@gmail.com>
2026-03-27 08:31:44 +0300

    Скоректирован DTOs

--- MuseumServer/DTOs/MediaFileResponse.cs
+++ MuseumServer/DTOs/MediaFileResponse.cs
@@ -1,11 +1,9 @@
 ﻿namespace MuseumServer.DTOs
 {
-    public class MediaFileResponse
+    public class CreateMediaFileRequest
     {
-        public int MediaFileId { get; set; }
         public string FilePath { get; set; } = string.Empty;
-        public string MediaType { get; set; } = string.Empty;
         public string? Description { get; set; }
-        public DepartmentInfo Department { get; set; } = new DepartmentInfo();
+        public int DepartmentId { get; set; }
     }
 }
