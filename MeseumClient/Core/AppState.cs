2be55507aa5df1f358501e304afe70d90262712a
Герман Исаев <germanisaev422@gmail.com>
2026-03-27 12:06:13 +0300

    Новый метод, который должен следить за всем и управлять представлениями

---
+++ MeseumClient/Core/AppState.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MeseumClient.Core
+{
+    public class AppState : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private object? _currentView;
+        public object? CurrentView
+        {
+            get => _currentView;
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        private string _userType = "guest";
+        public string UserType
+        {
+            get => _userType;
+            set { _userType = value; OnPropertyChanged(); }
+        }
+
+        private string? _token;
+        public string? Token
+        {
+            get => _token;
+            set { _token = value; OnPropertyChanged(); }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
