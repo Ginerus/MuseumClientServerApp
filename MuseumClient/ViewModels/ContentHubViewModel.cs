664a784283fa602373acbc0e149431925bd6cc0e
Герман Исаев <germanisaev422@gmail.com>
2026-03-30 07:34:12 +0300

    Команды для кнопок, отображается MessageBox с названием вкладки

--- MuseumClient/ViewModels/ContentHubViewModel.cs
+++ MuseumClient/ViewModels/ContentHubViewModel.cs
@@ -25,9 +25,13 @@ namespace MuseumClient.ViewModels
 
         // Команды для кнопок
         public RelayCommand ShowAboutMuseumCommand { get; }
-        //public RelayCommand ShowDepartmentsCommand { get; }
-        //public RelayCommand ShowDocumentsCommand { get; }
+        public RelayCommand ShowArticlesCommand { get; }
+        public RelayCommand ShowExhibitsCommand { get; }
+        public RelayCommand ShowDepartmentsCommand { get; }
+        public RelayCommand ShowIllustrationsCommand { get; }
+        public RelayCommand ShowVideosCommand { get; }
         public RelayCommand ExitCommand { get; }
+
 
         public ContentHubViewModel(MainViewModel mainVM)
         {
@@ -45,22 +49,45 @@ namespace MuseumClient.ViewModels
                 await AboutMuseumVM.LoadDepartmentCountAsync(); // обновляем данные
             });
 
+            ShowArticlesCommand = new RelayCommand(async _ =>
+            {
+                MessageBox.Show("Статьи");
+                //CurrentTabView = DepartmentVM;
+                //await Task.CompletedTask;
+            });
+
+            ShowExhibitsCommand = new RelayCommand(async _ =>
+            {
+                MessageBox.Show("Экспонаты");
+                //CurrentTabView = DepartmentVM;
+                //await Task.CompletedTask;
+            });
+
+            ShowDepartmentsCommand = new RelayCommand(async _ =>
+            {
+                MessageBox.Show("Отделы");
+                //CurrentTabView = DepartmentVM;
+                //await Task.CompletedTask;
+            });
+
+            ShowIllustrationsCommand = new RelayCommand(async _ =>
+            {
+                MessageBox.Show("Иллюстрации");
+                //CurrentTabView = DepartmentVM;
+                //await Task.CompletedTask;
+            });
+
+            ShowVideosCommand = new RelayCommand(async _ =>
+            {
+                MessageBox.Show("Видео");
+                //CurrentTabView = DepartmentVM;
+                //await Task.CompletedTask;
+            });
+
             ExitCommand = new RelayCommand(async _ =>
             {
                 _mainVM.ShowLoginView();
             });
-
-            //ShowDepartmentsCommand = new RelayCommand(async _ =>
-            //{
-            //    CurrentTabView = DepartmentVM;
-            //    await Task.CompletedTask;
-            //});
-
-            //ShowDocumentsCommand = new RelayCommand(async _ =>
-            //{
-            //    CurrentTabView = DocumentVM;
-            //    await Task.CompletedTask;
-            //});
 
             // Стартовая вкладка
             CurrentTabView = AboutMuseumVM;
