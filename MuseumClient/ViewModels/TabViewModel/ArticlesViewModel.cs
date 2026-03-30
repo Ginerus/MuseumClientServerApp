1521f661b1e6ebfc83a769bac6b1cc2ca37441ca
Герман Исаев <germanisaev422@gmail.com>
2026-03-30 07:58:13 +0300

    Перенесены tab'ы в свою папку, для удобства Представление и ViewModel для Статей

---
+++ MuseumClient/ViewModels/TabViewModel/ArticlesViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MuseumClient.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using MuseumClient.Commands;
+using System.Windows;
+using MuseumClient.Views;
+
+
+namespace MuseumClient.ViewModels
+{
+    public class ArticlesViewModel : INotifyPropertyChanged
+    {
+        private readonly ApiService _apiService;
+
+        public RelayCommand LoadCommand { get; }
+
+        public ArticlesViewModel() 
+        {
+            // Получаем настройки сервера из ConfigService
+            var config = new ConfigService().Server;
+
+            // Создаём ApiService с токеном из AuthService
+            _apiService = new ApiService(config, AuthService.Instance());
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
