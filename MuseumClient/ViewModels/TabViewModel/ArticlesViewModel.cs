605890c1ebb11cde84b837dcb180da3d9ccaa5dd
Герман Исаев <germanisaev422@gmail.com>
2026-03-31 11:40:20 +0300

    Работает отображение статей и отображение их в виде  плиток и их группировка

--- MuseumClient/ViewModels/TabViewModel/ArticlesViewModel.cs
+++ MuseumClient/ViewModels/TabViewModel/ArticlesViewModel.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MuseumClient.Services;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MuseumClient.Services;
 using MuseumClient.Commands;
+using MuseumClient.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
 using System.Windows;
-using MuseumClient.Views;
-
+using System.Windows.Data;
 
 namespace MuseumClient.ViewModels
 {
@@ -20,19 +13,66 @@ namespace MuseumClient.ViewModels
     {
         private readonly ApiService _apiService;
 
+        // Коллекция документов
+        public ObservableCollection<DocumentDto> Documents { get; set; } = new();
+
+        // View для группировки
+        private ICollectionView _documentsView;
+        public ICollectionView DocumentsView
+        {
+            get => _documentsView;
+            set
+            {
+                _documentsView = value;
+                OnPropertyChanged(nameof(DocumentsView));
+            }
+        }
+
         public RelayCommand LoadCommand { get; }
 
-        public ArticlesViewModel() 
+        public ArticlesViewModel()
         {
-            // Получаем настройки сервера из ConfigService
             var config = new ConfigService().Server;
-
-            // Создаём ApiService с токеном из AuthService
             _apiService = new ApiService(config, AuthService.Instance());
 
             LoadCommand = new RelayCommand(async _ => await LoadArticlesListAsync());
         }
 
+        // агрузка списка документов
+        public async Task LoadArticlesListAsync()
+        {
+            try
+            {
+                var response = await _apiService.GetAsync<DocumentsResponse>("Document");
+
+                Documents.Clear();
+
+                if (response?.Data != null)
+                {
+                    foreach (var doc in response.Data)
+                    {
+                        Documents.Add(doc);
+                    }
+                }
+
+                // Создаём CollectionView
+                DocumentsView = CollectionViewSource.GetDefaultView(Documents);
+
+                // Группировка по отделу
+                DocumentsView.GroupDescriptions.Clear();
+                DocumentsView.GroupDescriptions.Add(
+                    new PropertyGroupDescription(nameof(DocumentDto.DepartmentName))
+                );
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки статей: {ex.Message}");
+            }
+        }
+
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
