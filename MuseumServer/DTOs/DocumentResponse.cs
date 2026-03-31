216c32d7df4fec363e0092d11fffa1b68bde2fbf
Герман Исаев <germanisaev422@gmail.com>
2026-03-31 06:58:55 +0300

    Добавленно поле названия для dto response документа

--- MuseumServer/DTOs/DocumentResponse.cs
+++ MuseumServer/DTOs/DocumentResponse.cs
@@ -3,6 +3,7 @@
     public class DocumentResponse
     {
         public int DocumentId { get; set; }
+        public string Title { get; set; } = string.Empty;
         public string FilePath { get; set; } = string.Empty;
         public string FileType { get; set; } = string.Empty;
         public int? ExhibitId { get; set; }
