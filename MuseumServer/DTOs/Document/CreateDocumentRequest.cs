21aa7d70390178fc47788210015316f13b9386d1
Герман Исаев <germanisaev422@gmail.com>
2026-04-26 02:05:52 +0300

    Перемести в папку для удобства и починил отображение данных в сервисе

---
+++ MuseumServer/DTOs/Document/CreateDocumentRequest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace MuseumServer.DTOs
+{
+    public class CreateDocumentRequest
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public IFormFile File { get; set; } = null!;
+
+        public int? ExhibitId { get; set; }
+        public int? DepartmentId { get; set; }
+    }
+}
