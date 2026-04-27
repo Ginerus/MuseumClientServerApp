dac7219c785b6483f0991da57bbf1b800a953288
Герман Исаев <germanisaev422@gmail.com>
2026-04-27 13:26:23 +0300

    Отдельный dto для документов с отделом внутри

---
+++ MuseumServer/DTOs/Document/DocumentWithDepartmentResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace MuseumServer.DTOs
+{
+    public class DocumentWithDepartmentResponse
+    {
+        public int DocumentId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string FileType { get; set; } = string.Empty;
+
+        public DepartmentInfo? Department { get; set; }
+    }
+}
