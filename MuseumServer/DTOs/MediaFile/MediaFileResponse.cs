1900469dbf42f860a29088b01e72604bb619dd67
Герман Исаев <germanisaev422@gmail.com>
2026-04-28 04:26:14 +0300

    Дополнение нового столбца с навзанием файла В dto где он должен быть в меди файлах В вывод для отображения отделов

--- MuseumServer/DTOs/MediaFile/MediaFileResponse.cs
+++ MuseumServer/DTOs/MediaFile/MediaFileResponse.cs
@@ -3,6 +3,7 @@
     public class MediaFileResponse
     {
         public int MediaFileId { get; set; } // Автоинкремент
+        public string Title { get; set; } = string.Empty;
         public string MediaType { get; set; } = string.Empty;
     }
 }
