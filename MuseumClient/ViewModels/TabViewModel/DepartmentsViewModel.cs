f65d373e51969ea31ca691b44aed9311f4ddd2e3
Герман Исаев <germanisaev422@gmail.com>
2026-04-28 09:31:43 +0300

    Новый файл DepartmentsViewModel с содержимым

---
+++ MuseumClient/ViewModels/TabViewModel/DepartmentsViewModel.cs
@@ -0,0 +1,123 @@
+﻿using MuseumClient.Services;
+using MuseumClient.Commands;
+using MuseumClient.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace MuseumClient.ViewModels
+{
+    public class DepartmentsViewModel : INotifyPropertyChanged
+    {
+        private readonly ApiService _apiService;
+
+        public ObservableCollection<DepartmentDto> Departments { get; } = new();
+
+        private bool _isLoading;
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set
+            {
+                _isLoading = value;
+                OnPropertyChanged(nameof(IsLoading));
+            }
+        }
+
+        public RelayCommand LoadCommand { get; }
+        public RelayCommand OpenDepartmentCommand { get; }
+
+        public DepartmentsViewModel()
+        {
+            var config = new ConfigService().Server;
+            _apiService = new ApiService(config, AuthService.Instance());
+
+            LoadCommand = new RelayCommand(async _ => await LoadDepartmentsAsync());
+            OpenDepartmentCommand = new RelayCommand(async param => await OpenDepartment(param));
+        }
+
+        // 🔥 Загрузка списка отделов
+        public async Task LoadDepartmentsAsync()
+        {
+            try
+            {
+                IsLoading = true;
+
+                var response = await _apiService.GetAsync<DepartmentsResponse>("Department");
+
+                Departments.Clear();
+
+                if (response?.Data != null)
+                {
+                    foreach (var item in response.Data)
+                    {
+                        Departments.Add(item);
+                    }
+
+                    // 🔥 загрузка картинок
+                    _ = LoadThumbnailsAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки отделов: {ex.Message}");
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+
+        // 🔥 Клик по отделу
+        private async Task OpenDepartment(object? parameter)
+        {
+            if (parameter is not DepartmentDto dept)
+                return;
+
+            await Task.Run(() =>
+            {
+                MessageBox.Show(
+                    $"Отдел:\n{dept.Name}\nID: {dept.DepartmentId}"
+                );
+            });
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        // 🔥 Загрузка картинок с token
+        private async Task LoadThumbnailsAsync()
+        {
+            foreach (var dept in Departments)
+            {
+                try
+                {
+                    var bytes = await _apiService.GetBytesAsync($"Department/image/{dept.DepartmentId}");
+
+                    var image = new BitmapImage();
+
+                    using (var ms = new MemoryStream(bytes))
+                    {
+                        image.BeginInit();
+                        image.CacheOption = BitmapCacheOption.OnLoad;
+                        image.StreamSource = ms;
+                        image.EndInit();
+                        image.Freeze();
+                    }
+
+                    dept.ThumbnailImage = image;
+                }
+                catch
+                {
+                    // можно поставить placeholder позже
+                }
+            }
+        }
+    }
+}
