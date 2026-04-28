9ba04ed85af8ab3a88f2ca584710807572600ac9
Герман Исаев <germanisaev422@gmail.com>
2026-04-28 04:40:45 +0300

    Новый dto для загрузки медиа файла с клиента

--- MuseumServer/DTOs/MediaFile/CreateMediaFileRequest.cs
+++ MuseumServer/DTOs/MediaFile/CreateMediaFileRequest.cs
@@ -3,7 +3,7 @@
     public class CreateMediaFileRequest
     {
         public string Title { get; set; } = string.Empty;
-        public string FilePath { get; set; } = string.Empty;
+        public IFormFile File { get; set; } = null!;
         public string? Description { get; set; }
         public int DepartmentId { get; set; }
     }
