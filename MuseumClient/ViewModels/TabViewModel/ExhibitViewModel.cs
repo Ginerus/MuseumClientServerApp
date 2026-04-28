8aba3e0e4e9f326a19c8f9104d979b57a6d3b990
Герман Исаев <germanisaev422@gmail.com>
2026-04-28 06:24:01 +0300

    Базовый функционал в mv для Exhibit

--- MuseumClient/ViewModels/TabViewModel/ExhibitViewModel.cs
+++ MuseumClient/ViewModels/TabViewModel/ExhibitViewModel.cs
@@ -1,12 +1,120 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using MuseumClient.Services;
+using MuseumClient.Commands;
+using MuseumClient.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System;
 
 namespace MuseumClient.ViewModels
 {
-    internal class ExhibitViewModel
+    public class ExhibitsViewModel : INotifyPropertyChanged
     {
+        private readonly ApiService _apiService;
+
+        public ObservableCollection<ExhibitDto> Exhibits { get; } = new();
+
+        private ICollectionView? _exhibitsView;
+        public ICollectionView? ExhibitsView
+        {
+            get => _exhibitsView;
+            private set
+            {
+                _exhibitsView = value;
+                OnPropertyChanged(nameof(ExhibitsView));
+            }
+        }
+
+        private bool _isLoading;
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set
+            {
+                _isLoading = value;
+                OnPropertyChanged(nameof(IsLoading));
+            }
+        }
+
+        public RelayCommand LoadCommand { get; }
+        public RelayCommand OpenExhibitCommand { get; }
+
+        public ExhibitsViewModel()
+        {
+            var config = new ConfigService().Server;
+            _apiService = new ApiService(config, AuthService.Instance());
+
+            LoadCommand = new RelayCommand(async _ => await LoadExhibitsAsync());
+            OpenExhibitCommand = new RelayCommand(async param => await OpenExhibit(param));
+        }
+
+        // Загрузка экспонатов
+        public async Task LoadExhibitsAsync()
+        {
+            try
+            {
+                IsLoading = true;
+
+                var response = await _apiService.GetAsync<ExhibitsResponse>("Exhibit");
+
+                Exhibits.Clear();
+
+                if (response?.Data != null)
+                {
+                    foreach (var item in response.Data)
+                    {
+                        Exhibits.Add(item);
+                    }
+                }
+
+                SetupCollectionView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки экспонатов: {ex.Message}");
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+
+        // Группировка по отделам
+        private void SetupCollectionView()
+        {
+            ExhibitsView = CollectionViewSource.GetDefaultView(Exhibits);
+
+            if (ExhibitsView != null)
+            {
+                using (ExhibitsView.DeferRefresh())
+                {
+                    ExhibitsView.GroupDescriptions.Clear();
+                    ExhibitsView.GroupDescriptions.Add(
+                        new PropertyGroupDescription(nameof(ExhibitDto.DepartmentName))
+                    );
+                }
+            }
+        }
+
+        // Клик по экспонату
+        private async Task OpenExhibit(object? parameter)
+        {
+            if (parameter is not ExhibitDto exhibit)
+                return;
+
+            await Task.Run(() =>
+            {
+                MessageBox.Show(
+                    $"Экспонат:\n{exhibit.Name}\nОтдел: {exhibit.DepartmentName}\nID: {exhibit.ExhibitId}"
+                );
+            });
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
