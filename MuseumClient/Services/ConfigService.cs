655c2ac5c8317963339e2d61bce94d4f238ff765
Герман Исаев <germanisaev422@gmail.com>
2026-06-07 12:39:40 +0300

    Удаленно часть со streaming отедьным, так как данны проходят по стандартному url без допов

--- MuseumClient/Services/ConfigService.cs
+++ MuseumClient/Services/ConfigService.cs
@@ -6,7 +6,6 @@ namespace MuseumClient.Services
     public class ConfigService
     {
         public ServerConfig Server { get; private set; }
-        public StreamingConfig Streaming { get; private set; }
 
         public ConfigService()
         {
@@ -20,22 +19,10 @@ namespace MuseumClient.Services
                 LocalUrl = root.GetProperty("Server").GetProperty("LocalUrl").GetString(),
                 RemoteUrl = root.GetProperty("Server").GetProperty("RemoteUrl").GetString()
             };
-
-            Streaming = new StreamingConfig
-            {
-                LocalUrl = root.GetProperty("Streaming").GetProperty("LocalUrl").GetString(),
-                RemoteUrl = root.GetProperty("Streaming").GetProperty("RemoteUrl").GetString()
-            };
         }
     }
 
     public class ServerConfig
-    {
-        public string LocalUrl { get; set; }
-        public string RemoteUrl { get; set; }
-    }
-
-    public class StreamingConfig
     {
         public string LocalUrl { get; set; }
         public string RemoteUrl { get; set; }
