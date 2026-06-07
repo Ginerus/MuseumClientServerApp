6f707a81f745bffcb8c02a1732ce0ce6fb11b676
Герман Исаев <germanisaev422@gmail.com>
2026-06-07 15:24:29 +0300

    Глазик для скрытия пароля, работате и интерактивно скрывается. Он помщеён на поле пароля справа

---
+++ MuseumClient/Helpers/EyeIconConverter.cs
@@ -0,0 +1,22 @@
+﻿using MahApps.Metro.IconPacks;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MuseumClient.Helpers
+{
+    public class EyeGlyphConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool visible = (bool)value;
+
+            return visible
+                ? PackIconMaterialKind.EyeOff
+                : PackIconMaterialKind.Eye;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
