fe053b0ab8878aeb44fab5f3217edab7e3cba753
Герман Исаев <germanisaev422@gmail.com>
2026-06-13 00:04:02 +0300

    Удалён старый проект, который неудачный клиент

--- MeseumClientOld/Config/ServerConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MeseumClient.Config
-{
-    public class ServerConfig
-    {
-        public string Ip { get; set; } = null!; // не может быть null
-        public int Port { get; set; }
-    }
-}
