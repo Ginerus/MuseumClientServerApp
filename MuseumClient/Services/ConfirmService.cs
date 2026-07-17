52f5cdc754b5bc7d7ef4f7922b8fa9ce4b8df93f
Герман Исаев <germanisaev422@gmail.com>
2026-07-17 03:03:40 +0300

    Новый сервис для вызова окн подтверждения действий

---
+++ MuseumClient/Services/ConfirmService.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace MuseumClient.Services
+{
+    public static class ConfirmService
+    {
+        public static bool ConfirmDelete(string itemName = "эту статью")
+        {
+            var result = MessageBox.Show(
+                $"Вы действительно хотите удалить {itemName}?",
+                "Подтверждение удаления",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Warning);
+
+            return result == MessageBoxResult.Yes;
+        }
+    }
+}
